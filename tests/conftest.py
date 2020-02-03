272eba22edba0c2a39bd77ff97c96338af989e80
Eugene Prilepin <esp.home@gmail.com>
2020-02-03 17:57:25 +0300

    add test_mpe

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+
+import pytest
+
+from skimage.data import retina
+from skimage.color import rgb2gray
+from skimage.transform import rescale
+from skimage.filters import sato
+from skimage.exposure import adjust_log
+
+
+@pytest.fixture(scope='session')
+def speed_image_retina():
+    image_data = rescale(rgb2gray(retina())[260:1280, 90:800], 0.7)
+    speed_data = adjust_log(sato(image_data), gain=5.)
+
+    return speed_data
---
+++ tests/test_mpe.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import pytest
+
+from mpe import mpe
+
+
+@pytest.mark.parametrize('start_point, end_point, point_count', [
+    ((57, 395), (675, 455), 1127),
+])
+def test_extract_without_waypoints(caplog, speed_image_retina, start_point, end_point, point_count):
+    caplog.set_level(logging.DEBUG)
+    path_info = mpe(speed_image_retina, start_point=start_point, end_point=end_point)
+
+    assert path_info.point_count == point_count
