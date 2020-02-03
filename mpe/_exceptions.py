d2a28cdea9d08afe9a417b7833036787d5f21cf5
Eugene Prilepin <esp.home@gmail.com>
2020-02-03 04:19:51 +0300

    change exceptions parameters

--- mpe/_exceptions.py
+++ mpe/_exceptions.py
@@ -16,7 +16,15 @@ class ComputeTravelTimeError(MPEError):
 
 
 class ExtractPathError(MPEError):
-    pass
+    def __init__(self, *args,
+                 travel_time: np.ndarray,
+                 start_point: PointType,
+                 end_point: PointType) -> None:
+        super().__init__(*args)
+
+        self.travel_time = travel_time
+        self.start_point = start_point
+        self.end_point = end_point
 
 
 class EndPointNotReachedError(ExtractPathError):
@@ -26,10 +34,7 @@ class EndPointNotReachedError(ExtractPathError):
                  end_point: PointType,
                  extracted_points: List[FloatPointType],
                  last_distance: float) -> None:
-        super().__init__(*args)
+        super().__init__(*args, travel_time=travel_time, start_point=start_point, end_point=end_point)
 
-        self.travel_time = travel_time
-        self.start_point = start_point
-        self.end_point = end_point
         self.extracted_points = extracted_points
         self.last_distance = last_distance
