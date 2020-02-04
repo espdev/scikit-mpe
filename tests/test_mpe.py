45e904ed7a85026c9ce141d4c290b37b319b1c35
Eugene Prilepin <esp.home@gmail.com>
2020-02-04 04:03:10 +0300

    add test_mpe with waypoints

--- tests/test_mpe.py
+++ tests/test_mpe.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import collections
+
 import pytest
 
-from mpe import mpe
+from mpe import mpe, parameters
 
 
 @pytest.mark.parametrize('start_point, end_point, point_count', [
@@ -14,3 +16,24 @@ def test_extract_without_waypoints(caplog, speed_image_retina, start_point, end_
     path_info = mpe(speed_image_retina, start_point=start_point, end_point=end_point)
 
     assert path_info.point_count == point_count
+
+
+@pytest.mark.parametrize('start_point, end_point, way_points, ttime_cache, point_count, ttime_count, reversed_count', [
+    ((37, 255), (484, 300), ((172, 112), (236, 98), (420, 153)), True, 785, 2, 2),
+    ((37, 255), (484, 300), ((172, 112), (236, 98), (420, 153)), False, 785, 4, 0),
+])
+def test_extract_with_waypoints(caplog, speed_image_retina,
+                                start_point, end_point, way_points, ttime_cache,
+                                point_count, ttime_count, reversed_count):
+    caplog.set_level(logging.DEBUG)
+
+    with parameters(travel_time_cache=ttime_cache):
+        path_info = mpe(speed_image_retina,
+                        start_point=start_point, end_point=end_point, way_points=way_points)
+
+    assert path_info.point_count == point_count
+
+    ttime_counter = collections.Counter(id(piece.travel_time) for piece in path_info.pieces)
+    assert len(ttime_counter) == ttime_count
+
+    assert list(piece.reversed for piece in path_info.pieces).count(True) == reversed_count
