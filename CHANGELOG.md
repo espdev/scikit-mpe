d734f0fcc134d0df43be7594b5781daef1743aef
Eugene Prilepin <esp.home@gmail.com>
2020-02-06 16:03:17 +0300

    bump version to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v0.1.1
+
+- Fix links
+- Update readme
+
 ## v0.1.0
 
 - Initial release
--- skmpe/__init__.py
+++ skmpe/__init__.py
@@ -27,7 +27,7 @@ from ._mpe import MinimalPathExtractor
 import skmpe._api as _api  # noqa
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = [
     'InitialInfo',
